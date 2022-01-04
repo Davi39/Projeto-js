bd251f46fc88329abadfc4d823477e8e540e4b23
Davi Braga <davibragamoreira@hotmail.com>
2022-01-04 13:22:55 -0300

    main

---
+++ fundamentos/string.js
@@ -0,0 +1,10 @@
+const escola = "Arroz";
+
+console.log(escola.charAt(3));
+
+console.log('Doce de '.concat(escola).concat("!")); //concatenação usando o .concat, pode ser o "+" tbm
+console.log(escola.replace('z', 's')); // substituindo utilizando o replace
+
+console.log('Ana,Maria,Pedro'.split(',')); // separa em arrays 
+
+
