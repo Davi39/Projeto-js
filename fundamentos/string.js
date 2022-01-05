ec49ca4d73a530520d3d5006d51c75d5e7de4bf8
Davi Braga <davibragamoreira@hotmail.com>
2022-01-05 09:53:32 -0300

    Enter

--- fundamentos/string.js
+++ fundamentos/string.js
@@ -7,4 +7,20 @@ console.log(escola.replace('z', 's')); // substituindo utilizando o replace
 
 console.log('Ana,Maria,Pedro'.split(',')); // separa em arrays 
 
+//template string
 
+const nome = 'Rebeca';
+const concatenacao = 'Olá ' + nome + '!';
+const template = `
+    Olá 
+    ${nome}
+    !`
+
+console.log(concatenacao, template);
+
+//matemática
+
+console.log(`1 + 1 = ${1 + 1}`); // Dentro das crases e dentro do ${}, ele interpreta
+
+const up = texto => texto.toUpperCase();
+console.log(`Ei... ${up('cuidado')}`);
